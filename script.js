e63127322ec1b1b64c25858c0614181bdd072fc5
SAM <75918417+SAM-dev-pixel@users.noreply.github.com>
2022-03-19 09:13:04 +0700

    Adding animations

--- script.js
+++ script.js
@@ -7,4 +7,27 @@ const clickBurgerBtn =(btn)=> {
     (navLink.classList.contains('show')) ? btn.src = 'img/icon-close.svg' : btn.src = 'img/icon-hamburger.svg';
   });
 }
-clickBurgerBtn(document.querySelector('.burger-btn'))
+clickBurgerBtn(document.querySelector('.burger-btn'));
+
+// scroll animations
+const scrollAnimations =()=> {
+  
+  const vrGuyImage = document.querySelector('.vr-guy');
+  const vrText = document.querySelector('.vr-txt');
+  const vrPText = vrText.querySelector('p');
+  const creationsIncont = document.querySelectorAll('.creation-incont');
+  const footLinks = document.querySelectorAll('.foot-link');
+  const socialLinks = document.querySelectorAll('.social-link');
+  
+  const screenHeight = window.innerHeight / 1.4;
+  (vrGuyImage.getBoundingClientRect().top < screenHeight) ? vrGuyImage.classList.add('active') : vrGuyImage.classList.remove('active');
+  (vrText.getBoundingClientRect().top < screenHeight) ? vrText.classList.add('active') : vrText.classList.remove('active');
+  (vrPText.getBoundingClientRect().top < screenHeight) ? vrPText.classList.add('active') : vrPText.classList.remove('active');
+  creationsIncont.forEach(creation => (creation.getBoundingClientRect().top < screenHeight) ? creation.classList.add('active') : creation.classList.remove('active'));
+  footLinks.forEach(link => (link.getBoundingClientRect().top /1.35 < screenHeight) ? link.classList.add('active') : link.classList.remove('active'));
+  socialLinks.forEach(link => (link.getBoundingClientRect().top / 1.3 < screenHeight) ? link.classList.add('active') : link.classList.remove('active'));
+  
+}
+window.addEventListener('scroll', scrollAnimations);
+
+const introAnimate =()=> document.querySelector('.title-cont').classList.add('active');
